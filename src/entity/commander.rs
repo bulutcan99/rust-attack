5df5f3345897620cde10cc9f6041b0cce5e68caa
bulutcan99 <gocerbulutcan@gmail.com>
2024-12-01 18:04:27 +0300

    feat

--- src/entity/commander.rs
+++ src/entity/commander.rs
@@ -1,7 +1,50 @@
 use super::turret::Turret;
 use arrayvec::ArrayVec;
+use macroquad::math::Vec2;
 
 // First version will have one turret per commander
+#[derive(Debug)]
 pub struct Commander {
     turrets: ArrayVec<Turret, 1>,
+}
+
+impl Commander {
+    pub fn new(turrets: ArrayVec<Turret, 1>) -> Self {
+        return Self { turrets };
+    }
+
+    pub fn fire(&self, target: Vec2) -> Result<bool, anyhow::Error> {
+        let mut reachable_found = false;
+        for turret in self.turrets.iter() {
+            match self.target_reachable(turret.location, target) {
+                Ok(()) => {
+                    reachable_found = true;
+                    if turret.fire(target)? {
+                        return Ok(true); // Successfully hit
+                    }
+                }
+                Err(_) => continue, // Skip unreachable
+            }
+        }
+
+        if reachable_found {
+            Ok(false) // No turret could hit, but some were reachable
+        } else {
+            Err(anyhow::anyhow!("No turret can reach the target")) // No turret reachable
+        }
+    }
+
+    // Max 45 and 135 degrees are reachable
+    fn target_reachable(&self, location: Vec2, target: Vec2) -> Result<(), anyhow::Error> {
+        let vx = target.x - location.x;
+        let vy = target.y - location.y;
+        let theta = vy.atan2(vx);
+        let theta_degree = theta.to_degrees();
+        let theta_normalized = (theta_degree + 360.0) % 360.0;
+        if (45.0..=135.0).contains(&theta_normalized) {
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!("Target is not reachable"))
+        }
+    }
 }
--- src/entity/turret.rs
+++ src/entity/turret.rs
@@ -1,15 +1,21 @@
 use arrayvec::ArrayVec;
-use macroquad::math::Vec2;
+use macroquad::math::{bool, Vec2};
 
 use super::anti_missile::AntiMissile;
 
 // The commander will command the turrets
 #[derive(Debug)]
 pub struct Turret {
-    location: Vec2,
+    pub location: Vec2,
     muzzle_point: Vec2,
     anti_missiles: ArrayVec<AntiMissile, 1>,
     /// The maximum number of anti-missiles this turret can have in flight at a time.
     max_anti_missiles: u8,
     angle: f32,
+}
+
+impl Turret {
+    pub fn fire(&self, target: Vec2) -> Result<bool, anyhow::Error> {
+        Ok(true)
+    }
 }
